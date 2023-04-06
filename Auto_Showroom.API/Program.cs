3ea0069218f2479eb5be134548920c158444d9ea
Karayigit23 <karayigit833@gmail.com>
2023-04-06 14:03:30 +0300

    autoshowroom.core created"

---
+++ Auto_Showroom.API/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+builder.Services.AddDbContext<TestDbContext>(p => p.UseSqlServer("Server =localhost, 1433; Database = testDB10; User Id =SA; Password =MyPass@word;Persist Security Info=False;Encrypt=False"));
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ Auto_Showroom.Core/Model/Car.cs
@@ -0,0 +1,9 @@
+namespace Auto_Showroom.Core.Model;
+
+public class Car
+{
+    public int Id { get; set; } 
+    public decimal Price { get; set; }
+    public string Model { get; set; }
+    
+}
