72438fa157a049b41cffb9427fbde3616201df37
Karayigit23 <karayigit833@gmail.com>
2023-04-10 16:40:35 +0300

    order class fixed

--- Auto_Showroom.Core/Entity/Order.cs
+++ Auto_Showroom.Core/Entity/Order.cs
@@ -7,7 +7,7 @@ public class Order
     
     public int CarId { get; set; } //sipariş edilen Aracın Idsi
     public string PersonName { get; set; } //Sipariş eden kişinin adı
-    public Car Car { get; set; } //sipariş edilen araç
+    public Car Car { get; set; } //sipariş edilen araç 
     
     public List<OrderItem> OrderItems{ get; set; }//sipariş öğeleri
     public DateTime OrderDate { get; set; } // sipariş tarihi 
