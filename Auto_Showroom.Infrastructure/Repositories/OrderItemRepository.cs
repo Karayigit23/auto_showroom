2b73f4df899386a79838c03723eccb04a2874f3e
Karayigit23 <karayigit833@gmail.com>
2023-04-11 15:56:35 +0300

    OrderItemRepository.cs fixed

--- Auto_Showroom.Infrastructure/Repositories/OrderItemRepository.cs
+++ Auto_Showroom.Infrastructure/Repositories/OrderItemRepository.cs
@@ -1,12 +1,19 @@
 using Auto_Showroom.Core.Interfaces;
 using Auto_Showroom.Core.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace Auto_Showroom.Infrastructure.Repositories;
 
 public class OrderItemRepository : IOrderItemRepository
 {
+    private readonly AppDbContext _context;
+
+    public OrderItemRepository(AppDbContext context)
+    {
+        _context = context;
+    }
     public Task<List<OrderItem>> GetOrderItem()
     {
-        throw new NotImplementedException();
+        return _context.OrderItem.ToListAsync();
     }
 }
